1c5d0d99921bafc9ddf792aec84f0332b082e64f
Dave Dunkin <dave.dunkin@logos.com>
2020-09-16 17:45:00 -0700

    Initial commit.

---
+++ src/NewRelicDeploymentHook/AdmissionController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NewRelicDeploymentHook.k8s;
+
+namespace NewRelicDeploymentHook
+{
+	[ApiController]
+	[Route("")]
+	public class AdmissionController : ControllerBase
+	{
+		public AdmissionController(ILogger<AdmissionController> logger, IHttpClientFactory httpClientFactory)
+		{
+			m_logger = logger;
+			m_httpClientFactory = httpClientFactory;
+		}
+
+		[HttpPost]
+		[Route("mutate")]
+		public async Task<AdmissionReviewDto> Mutate(AdmissionReviewDto review)
+		{
+			var request = review.Request;
+			if (request is null)
+				throw new ArgumentException("request is required");
+			m_logger.LogDebug(JsonSerializer.Serialize(review, options: s_indentedCamelCaseOptions));
+			review.Response = new AdmissionResponseDto
+			{
+				Uid = request.Uid,
+				Allowed = true,
+			};
+			if (request.Kind?.Kind == "Deployment")
+			{
+				var patch = await MutateDeployment(request);
+				if (patch != null)
+				{
+					var patchJson = JsonSerializer.Serialize(patch, s_camelCaseOptions);
+					m_logger.LogInformation("Applying patch: {Patch}", patchJson);
+
+					review.Response.PatchType = PatchTypes.JsonPatchPatchType;
+					review.Response.Patch = Encoding.UTF8.GetBytes(patchJson);
+				}
+			}
+			return review;
+		}
+
+		private async Task<IReadOnlyList<JsonPatchOperation>?> MutateDeployment(AdmissionRequestDto request)
+		{
+			if (!(request.Object is JsonElement objectEl))
+			{
+				m_logger.LogWarning("Invalid object");
+				return null;
+			}
+
+			var metadataEl = objectEl.GetProperty("metadata");
+			if (!metadataEl.TryGetProperty("annotations", out var annotationsEl) || !annotationsEl.TryGetProperty("newRelic", out var newRelicEl))
+			{
+				m_logger.LogInformation("No newRelic metadata");
+				return null;
+			}
+
+			var newRelicMetadata = JsonSerializer.Deserialize<JsonElement>(newRelicEl.GetString());
+
+			var appId = newRelicMetadata.TryGetStringProperty("appId");
+			if (appId is null)
+			{
+				m_logger.LogInformation("No appId");
+				return null;
+			}
+
+			var revision = newRelicMetadata.TryGetStringProperty("revision");
+			if (revision is null)
+			{
+				m_logger.LogInformation("No revision");
+				return null;
+			}
+
+			var deployment = new DeploymentDto
+			{
+				Revision = revision,
+				User = (request.UserInfo?.Username),
+				// Omit timestamp. New Relic will respond with BadRequest if the timestamp is in the future by even 1 second.
+			};
+
+			var deploymentJson = JsonSerializer.Serialize(deployment, s_camelCaseOptions);
+
+			m_logger.LogInformation("New Relic Deployment: {Deployment}", deploymentJson);
+
+			var lastAppliedDeploymentJson = annotationsEl.TryGetStringProperty("lastAppliedNewRelicDeployment");
+			var lastAppliedDeployment = lastAppliedDeploymentJson is null ? default(JsonElement?) : JsonSerializer.Deserialize<JsonElement>(lastAppliedDeploymentJson);
+			var lastAppliedRevision = lastAppliedDeployment?.TryGetStringProperty("revision");
+
+			if (lastAppliedRevision == revision)
+			{
+				m_logger.LogInformation("No change from last deployment");
+				return null;
+			}
+
+			var deploymentResponse = await RecordDeploymentAsync(appId, deployment);
+			var patch = new[]
+			{
+				new JsonPatchOperation("add", "/metadata/annotations/lastAppliedNewRelicDeployment", JsonSerializer.Serialize(deploymentResponse)),
+			};
+			return patch;
+		}
+
+		private async Task<JsonElement?> RecordDeploymentAsync(string appId, DeploymentDto deployment)
+		{
+			using var client = m_httpClientFactory.CreateClient("newrelic");
+			var response = await client.PostAsJsonAsync($"applications/{appId}/deployments.json", new
+			{
+				deployment,
+			});
+			var responseContent = await response.Content.ReadFromJsonAsync<JsonElement>();
+			m_logger.LogInformation("Recorded deployment\n{StatusCode} {Reason}: {Content}", (int) response.StatusCode, response.ReasonPhrase, responseContent);
+			return response.IsSuccessStatusCode ? responseContent.GetProperty("deployment") : default(JsonElement?);
+		}
+
+		private static readonly JsonSerializerOptions s_camelCaseOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+		private static readonly JsonSerializerOptions s_indentedCamelCaseOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true };
+		private readonly ILogger<AdmissionController> m_logger;
+		private readonly IHttpClientFactory m_httpClientFactory;
+	}
+
+	internal static class JsonUtility
+	{
+		public static string? TryGetStringProperty(this JsonElement element, string propertyName)
+			=> element.TryGetProperty(propertyName, out var value) && value.ValueKind == JsonValueKind.String ? value.GetString() : null;
+	}
+
+	internal class DeploymentDto
+	{
+		public string? Revision { get; set; }
+		public string? Changelog { get; set; }
+		public string? Description { get; set; }
+		public string? User { get; set; }
+		public DateTime? Timestamp { get; set; }
+	}
+
+	internal class JsonPatchOperation
+	{
+		public JsonPatchOperation(string op, string path, string value)
+		{
+			Op = op;
+			Path = path;
+			Value = value;
+		}
+
+		public string Op { get; }
+		public string Path { get; }
+		public string Value { get; }
+
+		public override bool Equals(object? obj) => obj is JsonPatchOperation other && Op == other.Op && Path == other.Path && Value == other.Value;
+		public override int GetHashCode() => HashCode.Combine(Op, Path, Value);
+	}
+}
---
+++ src/NewRelicDeploymentHook/KubernetesDtos.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace NewRelicDeploymentHook.k8s
+{
+	public class AdmissionReviewDto
+	{
+		public string? Kind { get; set; }
+		public string? ApiVersion { get; set; }
+		public AdmissionRequestDto? Request { get; set; }
+		public AdmissionResponseDto? Response { get; set; }
+	}
+
+	public class KindDto
+	{
+		public string? Group { get; set; }
+		public string? Version { get; set; }
+		public string? Kind { get; set; }
+	}
+
+	public class ResourceDto
+	{
+		public string? Group { get; set; }
+		public string? Version { get; set; }
+		public string? Resource { get; set; }
+	}
+
+	public class UserInfoDto
+	{
+		public string? Username { get; set; }
+		public string? Uid { get; set; }
+		public List<string>? Groups { get; set; }
+	}
+
+	// AdmissionRequest describes the admission.Attributes for the admission request.
+	public class AdmissionRequestDto
+	{
+		// UID is an identifier for the individual request/response. It allows us to distinguish instances of requests which are
+		// otherwise identical (parallel requests, requests when earlier requests did not modify etc)
+		// The UID is meant to track the round trip (request/response) between the KAS and the WebHook, not the user request.
+		// It is suitable for correlating log entries between the webhook and apiserver, for either auditing or debugging.
+		public string? Uid { get; set; }
+		// Kind is the type of object being manipulated.  For example: Pod
+		public KindDto? Kind { get; set; }
+		// Resource is the name of the resource being requested.  This is not the kind.  For example: pods
+		public ResourceDto? Resource { get; set; }
+		// SubResource is the name of the subresource being requested.  This is a different resource, scoped to the parent
+		// resource, but it may have a different kind. For instance, /pods has the resource "pods" and the kind "Pod", while
+		// /pods/foo/status has the resource "pods", the sub resource "status", and the kind "Pod" (because status operates on
+		// pods). The binding resource for a pod though may be /pods/foo/binding, which has resource "pods", subresource
+		// "binding", and kind "Binding".
+		// +optional
+		public string? SubResource { get; set; }
+		// Name is the name of the object as presented in the request.  On a CREATE operation, the client may omit name and
+		// rely on the server to generate the name.  If that is the case, this method will return the empty string.
+		// +optional
+		public string? Name { get; set; }
+		// Namespace is the namespace associated with the request (if any).
+		// +optional
+		public string? Namespace { get; set; }
+		// Operation is the operation being performed
+		public string? Operation { get; set; }
+		// UserInfo is information about the requesting user
+		public UserInfoDto? UserInfo { get; set; }
+		// Object is the object from the incoming request prior to default values being applied
+		// +optional
+		public JsonElement? Object { get; set; }
+		// OldObject is the existing object. Only populated for UPDATE requests.
+		// +optional
+		public JsonElement? OldObject { get; set; }
+	}
+
+	// AdmissionResponse describes an admission response.
+	public class AdmissionResponseDto
+	{
+		// UID is an identifier for the individual request/response.
+		// This should be copied over from the corresponding AdmissionRequest.
+		public string? Uid { get; set; }
+		// Allowed indicates whether or not the admission request was permitted.
+		public bool? Allowed { get; set; }
+		// Result contains extra details into why an admission request was denied.
+		// This field IS NOT consulted in any way if "Allowed" is "true".
+		// +optional
+		public JsonElement? Result { get; set; }
+		// Patch contains the actual patch. Currently we only support a response in the form of JSONPatch, RFC 6902.
+		// +optional
+		public byte[]? Patch { get; set; }
+		// PatchType indicates the form the Patch will take. Currently we only support "JSONPatch".
+		// +optional
+		public string? PatchType { get; set; }
+	}
+
+	// PatchType is the type of patch being used to represent the mutated object
+	// PatchType constants.
+	public static class PatchTypes
+	{
+		public const string JsonPatchPatchType = "JSONPatch";
+	}
+
+	// Operation is the type of resource operation being checked for admission control
+	// Operation constants
+	public static class Opeartions
+	{
+		public const string CreateOperation = "CREATE";
+		public const string UpdateOperation = "UPDATE";
+		public const string DeleteOperation = "DELETE";
+		public const string ConnectOperation = "CONNECT";
+	}
+}
